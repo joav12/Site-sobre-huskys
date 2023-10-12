dddf720ec549d432bc8e55ff1dcb713a4d4c05fb
João Rosinhole <jvr131104@gmail.com>
2023-10-12 01:09:30 -0300

    Projeto finalizado

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ fechar_menu.addEventListener('click', ()=>{
     $('.menu').css("display", "none");
     $('body').css('overflow', "auto");
 });
+
+if(document.getElementById('butao_env') != null){
+    const butao_env = document.getElementById('butao_env');
+    
+    butao_env.addEventListener('click', ()=>{
+        const email = document.getElementById('email').value;
+        const idade_dogo = document.getElementById('idade_dogo').value;
+    
+        if(email == "" && idade_dogo == "" || email == "" || idade_dogo == ""){
+            alert('Por favor preencher campos obrigatórios');
+        }
+        else{
+            $('.idade_doquinho p').css('color','green');
+    
+            if(idade_dogo == 1){
+                $('.idade_doquinho p span').text(`${idade_dogo} ano`);
+            }
+            else{
+                $('.idade_doquinho p span').text(`${idade_dogo} anos`);
+            }
+        }
+    })
+}
